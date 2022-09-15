5cdcff1c0694ac4ef36c8ccb1c6897dac92f61a5
Lyle Son <calends99@gmail.com>
2022-09-15 23:11:08 +0900

    [programmers] 완주하지 못한 선수

---
+++ src/programmers/완주하지_못한_선수.java
@@ -0,0 +1,33 @@
+package programmers;
+
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String c : completion) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        for (String p : participant) {
+            if (!map.containsKey(p) || map.get(p) == 0) {
+                return p;
+            } else {
+                map.put(p, map.get(p) - 1);
+            }
+        }
+        return "";
+    }
+}
+
+public class 완주하지_못한_선수 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        System.out.println(solution.solution(participant, completion));
+    }
+}
